309f56fa22a0f3667de682d0404a80b179d6f69d
rizkyjackbar <muhamadrizky62020@students.amikom.ac.id>
2024-06-22 19:04:31 +0700

    change

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -10,7 +10,7 @@ function Home() {
     <div className="flex flex-col items-center justify-center min-h-screen" style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
       <div className="max-w-md w-full py-10 px-6 bg-white shadow-md lg:max-w-lg lg:mx-auto" style={{ backgroundColor: 'rgba(255, 255, 255, 0.5)', maxWidth: 'calc(55vw - -85px)', borderRadius: '20px', margin: '0 10px' }}>
         <h2 className="text-2xl text-center font-semibold mb-4">Welcome to DioPark</h2>
-        <p className="text-gray-700 text-center">Kita semua bercita-cita jadi kang parkir</p>
+        <p className="text-gray-700 text-center">One Think, One Solution</p>
         <div className="flex justify-center mt-4">
 
           <div className="flex justify-center mt-4 px-1">

d92ec7ac2d43ea5d382d1e15465c09e7802e4403
rizkyjackbar <muhamadrizky62020@students.amikom.ac.id>
2024-06-22 21:24:56 +0700

    make not protect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Register from './page/Register';
@@ -10,53 +9,17 @@ import UserProfile from './page/UserProfile';
 import History from './page/History';
 import ForgotPassword from './page/ForgetPassword';
 import UpdatePassword from './page/UpdatePassword';
-import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
-
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  useEffect(() => {
-    const accessToken = getCookie('token');
-    setIsAuthenticated(!!accessToken);
-  }, []);
-
-  const getCookie = (name) => {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop().split(';').shift();
-  };
-
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute isAuthenticated={isAuthenticated}>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/userprofile"
-          element={
-            <ProtectedRoute isAuthenticated={isAuthenticated}>
-              <UserProfile />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/history"
-          element={
-            <ProtectedRoute isAuthenticated={isAuthenticated}>
-              <History />
-            </ProtectedRoute>
-          }
-        />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/userprofile" element={<UserProfile />} />
+        <Route path="/history" element={<History />} />
         <Route path="/forgetpassword" element={<ForgotPassword />} />
         <Route path="/updatepassword" element={<UpdatePassword />} />
         <Route path="*" element={<NotFound />} />
